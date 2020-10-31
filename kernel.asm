af71c7241f44ab4160ad2b3c4a605edcb77dd7f9
lugorosito <lugorosito@dc.uba.ar>
2020-10-31 17:56:38 -0300

    Inicializar Pantalla en C

--- kernel.asm
+++ kernel.asm
@@ -6,7 +6,7 @@
 %include "print.mac"
 
 global start
-
+extern inicializar_pantalla
 
 BITS 16
 ;; Saltear seccion de datos
@@ -21,16 +21,16 @@ start_rm_len equ    $ - start_rm_msg
 start_pm_msg db     'Iniciando kernel en Modo Protegido'
 start_pm_len equ    $ - start_pm_msg
 
-%define GDT_IDX_C0_DESC   10<<3
-%define GDT_IDX_C3_DESC   11<<3
-%define GDT_IDX_D0_DESC   12<<3
-%define GDT_IDX_D3_DESC   13<<3
-%define GDT_IDX_VID_DESC  14<<3
 %define INIT_STACK 0x00025000
 %define SPACE 32
 %define TAMANO1 8000
 %define TAMANO2 6400
-extern GDT_DESC
+%define GDT_OFF_C0_DESC   10 << 3
+%define GDT_OFF_D0_DESC   11 << 3
+%define GDT_OFF_C3_DESC   12 << 3
+%define GDT_OFF_D3_DESC   13 << 3
+%define GDT_OFF_VID_DESC  14 << 3
+extern GDT_DESC 
 ;;
 ;; Seccion de código.
 ;; -------------------------------------------------------------------------- ;;
@@ -58,51 +58,60 @@ start:
     call A20_check
     call A20_enable
     call A20_check
+
     ; Cargar la GDT
     lgdt [GDT_DESC]
+
     ; Setear el bit PE del registro CR0
     mov eax,CR0
     or eax, 1
     mov CR0, eax
     ; Saltar a modo protegido
-    jmp GDT_IDX_C0_DESC:modo_protegido
+    jmp GDT_OFF_C0_DESC:modo_protegido
 
 BITS 32
 modo_protegido:
 
     ; Establecer selectores de segmentos
-    mov ax, GDT_IDX_D0_DESC
+    mov ax, GDT_OFF_D0_DESC
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
-    mov ss, ax ;esto anda, no?
+    mov ss, ax ;esto anda, no? si
+
     ; Establecer la base de la pila
     mov ebp, INIT_STACK
-    mov esp, INIT_STACK        ; esto está bien, no?
+    mov esp, INIT_STACK        ; esto está bien, no? si
+    
     ; Imprimir mensaje de bienvenida
     print_text_pm start_pm_msg, start_pm_len, 0x0F, 25, 25
+
     ; Inicializar pantalla
-    mov ax, GDT_IDX_VID_DESC
-    mov ds, ax
+    ;mov ax, GDT_OFF_VID_DESC  
+    ;mov ds, ax                
     
-    mov ecx, 0
-    mov edx, 0
-    .limpiar:
-        cmp ecx, TAMANO1
-        je .pintar
-        mov ax, 0x0020
-        mov [ds:ecx], ax
-        add ecx, 2
-        jmp .limpiar
-    .pintar:
-        cmp edx, TAMANO2
-        je .fin
-        mov ax, 0x5020
-        mov [ds:edx], ax
-        add edx, 2
-        jmp .pintar
-    .fin:
+    ;mov ecx, 0 ; contador 
+    ;mov edx, 0 ; contador 
+
+    ; Rutina para limpiar pantalla
+    ;.limpiar:
+        ;cmp ecx, TAMANO1
+        ;je .pintar
+        ;mov ax, 0x0020   ; fondo negro, letra negra, caracter espacio
+        ;mov [ds:ecx], ax
+        ;add ecx, 2
+        ;jmp .limpiar
+    ; Rutina para pintar pantalla    
+    ;.pintar:
+        ;cmp edx, TAMANO2
+        ;je .fin
+        ;mov ax, 0x5020 ; fondo magenta, letra negra, caracter espacio
+        ;mov [ds:edx], ax
+        ;add edx, 2
+        ;jmp .pintar
+
+    call inicializar_pantalla
     ; Inicializar el manejador de memoria
  
     ; Inicializar el directorio de paginas
